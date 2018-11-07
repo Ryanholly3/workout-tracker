9e5904e49398d06b338363422ffe07288557eb4c
ryanholly3 <ryanholly3@gmail.com>
2018-11-07 10:25:18 -0700

    added date field and submit button on swim input

--- src/components/swim/SwimInput.js
+++ src/components/swim/SwimInput.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import { Segment } from 'semantic-ui-react';
-import { Form, Input, TextArea } from 'semantic-ui-react';
+import { Form, Input, TextArea, Button } from 'semantic-ui-react';
 import '../../App.css';
 
 class SwimInput extends Component {
@@ -11,6 +11,10 @@ class SwimInput extends Component {
       <Segment raised>
         <h3>Record a swim</h3>
         <Form>
+          <Form.Field required>
+            <label>Date</label>
+            <Input type='date' />
+          </Form.Field>
           <Form.Field required>
             <label>Distance</label>
             <Input placeholder='in yards...' />
@@ -29,6 +33,7 @@ class SwimInput extends Component {
             label='Notes'
             placeholder='Notes'
           />
+          <Button type='submit'>Submit</Button>
         </Form>
       </Segment>
     );
